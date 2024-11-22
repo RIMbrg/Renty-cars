87562db59053e85f7fa1c117111a70f21c406319
rim bourogaa <rimobourogaa@gmil.com>
2024-11-22 02:10:35 +0100

    Commit

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,9 +1,12 @@
+// ignore_for_file: use_build_context_synchronously, prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Firestore import
 import 'package:flutter/material.dart';
 import 'package:renty_cars/widgets/ButtonAuth.dart';
 import 'package:renty_cars/widgets/CustomTextField.dart';
 import 'package:renty_cars/widgets/LogoAuth.dart';
 import 'package:renty_cars/widgets/MyAppBar.dart';
-import 'package:renty_cars/widgets/SocialButton.dart';
 import 'package:renty_cars/widgets/constants.dart';
 import 'package:renty_cars/widgets/navbar.dart';
 
@@ -17,6 +20,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController email = TextEditingController();
   TextEditingController password = TextEditingController();
+  String _userRole = '';
 
   @override
   Widget build(BuildContext context) {
@@ -28,10 +32,11 @@ class _LoginState extends State<Login> {
       ),
       body: SingleChildScrollView(
         child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 25),
-            height: MediaQuery.of(context).size.height - 50,
-            width: double.infinity,
-            child: ListView(children: [
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          height: MediaQuery.of(context).size.height - 50,
+          width: double.infinity,
+          child: ListView(
+            children: [
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -42,9 +47,11 @@ class _LoginState extends State<Login> {
                       children: [
                         Text('Login',
                             style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 30)),
+                                fontWeight: FontWeight.bold,
+                                fontSize: 30,
+                                color: Colors.deepOrange)),
                         SizedBox(height: 10),
-                        Text('Login to continue using the app',
+                        Text('Enter your login ',
                             style: TextStyle(color: Colors.grey)),
                       ],
                     ),
@@ -58,7 +65,8 @@ class _LoginState extends State<Login> {
                   CustomTextField(
                       fieldname: 'Password',
                       hinttext: "Enter your Password",
-                      mycontroller: password),
+                      mycontroller: password,
+                      ),
                   Container(
                     margin: const EdgeInsets.only(top: 10, bottom: 20),
                     alignment: Alignment.topRight,
@@ -73,46 +81,68 @@ class _LoginState extends State<Login> {
                 buttontext: 'Login',
                 ButtonColor: tAccentColor,
                 TextColor: tWhiteColor,
-                onpressed: () { Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => NavBarPage()));
+                onpressed: () async {
+                  try {
+                    final credential = await FirebaseAuth.instance
+                        .signInWithEmailAndPassword(
+                      email: email.text,
+                      password: password.text,
+                    );
 
+                    // Fetch the role from Firestore
+                    final userDoc = await FirebaseFirestore.instance
+                        .collection('users')
+                        .doc(credential.user!.uid)
+                        .get();
+
+                    if (userDoc.exists) {
+                      _userRole = userDoc['role']; // Fetch user role
+
+                      // Navigate based on role
+                      if (_userRole == 'User') {
+                         Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => const   NavBarPage()),
+                        );
+                      } else if (_userRole == 'Agent') {
+                       
+                      }
+                    } else {
+                      // Handle the case when user document is not found
+                      print('User document not found');
+                    }
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'user-not-found') {
+                      print('No user found for that email.');
+                    } else if (e.code == 'wrong-password') {
+                      print('Wrong password provided.');
+                    }
+                  } catch (e) {
+                    print(e);
+                  }
                 },
-              ),
-              const SizedBox(height: 20),
-              const Text(
-                'Or Login with',
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SocialButton(imagepath: 'images/google.png'),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  SocialButton(imagepath: 'images/facebook.png'),
-                ],
               ),
               const SizedBox(height: 50),
               InkWell(
                 onTap: () {
-                  Navigator.of(context).pushReplacementNamed("signup");
+                  Navigator.of(context).pushNamed("signup");
                 },
                 child: const Text.rich(
                   TextSpan(children: [
                     TextSpan(text: "Don't have an account?"),
                     TextSpan(
-                        text: " Sign up",
+                        text: " Sign Up",
                         style: TextStyle(color: Color(0xFF2972FF)))
                   ]),
                   textAlign: TextAlign.center,
                 ),
               ),
-            ])),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
